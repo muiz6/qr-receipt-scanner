fb80e40a10d443f95bc1484f8f3c3af0619a5e3e
muiz6 <muizhassan83@gmail.com>
2022-03-13 20:41:05 +0500

    merged in search tab layout

---
+++ QrReceiptScanner/app/src/main/java/com/example/qrreceiptscanner/SearchTab.java
@@ -0,0 +1,14 @@
+package com.example.qrreceiptscanner;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchTab extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_search_tab);
+	}
+}
